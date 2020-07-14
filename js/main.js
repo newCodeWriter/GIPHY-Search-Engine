52393349d5f8e706762e339258ce802903b88d66
Shay Brown <fire328@ymail.com>
2020-07-14 12:58:51 -0400

    remove duplicate code for mobile view

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 $(function(){
-    var mobSearch = $('#search-field').clone().addClass('mobile-search').removeAttr('id');
-    
     function aangGifs(){   
         var input = 'aang avatar';
         $('.search-btn').focus();
@@ -26,62 +24,30 @@ $(function(){
 
     $('.search-btn').click(function(){
         if($(window).width() < 571){
-            $('nav').after(mobSearch);
-            $('.mobile-search > input').removeClass('search-input').addClass('mobile-input').attr('size', 22);
-            $('.mobile-search > button').addClass('mobile-btn');
-            $('.mobile-btn').text('').append('<i class="fas fa-search"></i>');
+            $('.mobile-search').show();
+        }
+        else if($(window).width() > 571){
+            $('.search-input').show();
         }
     })
 
-    $(window).resize(function(){
+   $(window).resize(function(){
         if($(this).width() > 571){
-            $('.toggle-menu').show();
-            $('.mobile-search').remove();
+            $('.mobile-search').hide();
         }
         else if($(this).width() < 571){
             $('.search-input').hide();
         }
     })
 
-    $('.search-it').delegate('.search-btn', 'click', function(){
-        if($(window).width() > 571){    
-            $('.search-input').show();
-            if($('.search-input').val() == ''){
-                $('.search-input').focus();
-            }
-            else{
-                var userInput = $('.search-input').val();
-                $('#gif-search-btn').text(userInput);
-                $.ajax({
-                    url: `https://api.giphy.com/v1/gifs/search?api_key=sL5xWiHQ5a6PSB0LoE6A6t2yovo19jxp&q=${userInput}&limit=30`,
-                    success: function(){$('.source-link').remove();}
-                }).done(function(res){
-                    var gifs = res.data; 
-                    if(gifs.length == ""){
-                        $('#gif-items-btn').text(0);
-                        $('.search-input').val('');
-                    } 
-                    else{
-                        $.each(gifs, function(i, e){
-                            var img = e.images.original.url;
-                            var source = e.source; 
-                            $('.insert-gifs').append(`<a href="${source}" target="_blank" class="source-link"><img src="${img}" class="new-img" alt="a gif"/></a>`);
-                            $('.search-input').val('');
-                            $('#gif-items-btn').text(gifs.length);
-                        })
-                    }  
-                })
-            }
-        }
-    })
-
-    $('body').delegate('.mobile-search > .mobile-btn', 'click', function(){
-        if($('.mobile-input').val() == ''){
-            $('.mobile-input').focus();
+    $('body').delegate('.search-it > .search-btn', 'click', function(){
+        if($(this).siblings().val() == ''){
+            $('.search-it > input').focus();
         }
         else{
-            var userInput = $('.mobile-input').val();
+            var userInput = $(this).siblings().val();
             $('#gif-search-btn').text(userInput);
+            //remove gifs from previous search
             $('.source-link').remove(); 
             $.ajax({
                 url: `https://api.giphy.com/v1/gifs/search?api_key=sL5xWiHQ5a6PSB0LoE6A6t2yovo19jxp&q=${userInput}&limit=30`
@@ -89,20 +55,21 @@ $(function(){
                 var gifs = res.data;  
                 if(gifs.length == ""){
                     $('#gif-items-btn').text(0);
-                    $('.mobile-input').val('');
                 }
                 else{
                     $.each(gifs, function(i, e){
                         var img = e.images.original.url;
+                        //add source so that user can click on gif and be directed to the source of the gif in a new tab
                         var source = e.source; 
                         $('.insert-gifs').append(`<a href="${source}" target="_blank" class="source-link"><img src="${img}" class="new-img" alt="a gif"/></a>`);
-                        $('.mobile-input').val('');
                         $('#gif-items-btn').text(gifs.length);
                     })
                 }   
             }) 
+            $(this).siblings().val('');
         }
     })
+    //Remove href attribute for gifs with no source
     $('.insert-gifs').delegate("a[href='']", "mouseover mouseenter", function(){
         $(".insert-gifs > a[href='']").removeAttr('href');
     })
