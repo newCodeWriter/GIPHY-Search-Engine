ca07bee618c6625640db16d574f8bc680b45a03f
Shay Brown <fire328@ymail.com>
2021-02-10 05:39:51 -0500

    clean up; formatting;

--- js/main.js
+++ js/main.js
@@ -1,76 +1,75 @@
-$(function(){
-    function aangGifs(){   
-        var input = 'aang avatar';
-        $('.search-btn').focus();
-        $('#gif-search-btn').text(input);
-        $.ajax({
-            url: `https://api.giphy.com/v1/gifs/search?api_key=sL5xWiHQ5a6PSB0LoE6A6t2yovo19jxp&q=${input}&limit=30`
-        }).done(function(res){
-            var gifs = res.data;  
-            $.each(gifs, function(i, e){
-                var img = e.images.original.url;
-                var source = e.source; 
-                $('.insert-gifs').append(`<a href="${source}" target="_blank" class="source-link"><img src="${img}" class="new-img" alt="a gif"/></a>`);
-                $('#gif-items-btn').text(gifs.length);
-            })
-        })
-    }
+/* eslint-disable comma-dangle */
+/**
+ * /* eslint-disable vars-on-top
+ *
+ * /* eslint-disable prefer-arrow-callback */
 
-    aangGifs();
-    
-    $('.menu-btn').click(function(){
-        $('.toggle-menu').toggle();
-    })
+/* eslint-disable no-undef */
+/* eslint-disable func-names */
+/* eslint-disable no-tabs */
+/* eslint-disable indent */
+/** @format */
 
-    $('.search-btn').click(function(){
-        if($(window).width() < 571){
-            $('.mobile-search').show();
-        }
-        else if($(window).width() > 571){
-            $('.search-input').show();
-        }
-    })
+// eslint-disable-next-line prefer-arrow-callback
+$(function () {
+	const yellow = () => {
+		const input = 'yellow';
+		$('.search-input').focus();
+		$('#gif-search').text(input);
+		$.ajax({
+			url: `https://api.giphy.com/v1/gifs/search?api_key=sL5xWiHQ5a6PSB0LoE6A6t2yovo19jxp&q=${input}&limit=30`,
+		}).done((res) => {
+			const { data } = res;
+			$.each(data, (i, e) => {
+				const {
+					source,
+					title,
+					images: { original },
+				} = e;
+				$('.insert-gifs').append(
+					`<a href="${source}" target="_blank" class="source-link"><img src="${original.url}" class="new-img" alt="${title}"/></a>`
+				);
+				$('#gif-items').text(`${data.length} GIFs`);
+			});
+		});
+	};
 
-   $(window).resize(function(){
-        if($(this).width() > 571){
-            $('.mobile-search').hide();
-        }
-        else if($(this).width() < 571){
-            $('.search-input').hide();
-        }
-    })
+	yellow();
 
-    $('body').delegate('.search-it > .search-btn', 'click', function(){
-        if($(this).siblings().val() == ''){
-            $('.search-it > input').focus();
-        }
-        else{
-            var userInput = $(this).siblings().val();
-            $('#gif-search-btn').text(userInput);
-            //remove gifs from previous search
-            $('.source-link').remove(); 
-            $.ajax({
-                url: `https://api.giphy.com/v1/gifs/search?api_key=sL5xWiHQ5a6PSB0LoE6A6t2yovo19jxp&q=${userInput}&limit=30`
-            }).done(function(res){
-                var gifs = res.data;  
-                if(gifs.length == ""){
-                    $('#gif-items-btn').text(0);
-                }
-                else{
-                    $.each(gifs, function(i, e){
-                        var img = e.images.original.url;
-                        //add source so that user can click on gif and be directed to the source of the gif in a new tab
-                        var source = e.source; 
-                        $('.insert-gifs').append(`<a href="${source}" target="_blank" class="source-link"><img src="${img}" class="new-img" alt="a gif"/></a>`);
-                        $('#gif-items-btn').text(gifs.length);
-                    })
-                }   
-            }) 
-            $(this).siblings().val('');
-        }
-    })
-    //Remove href attribute for gifs with no source
-    $('.insert-gifs').delegate("a[href='']", "mouseover mouseenter", function(){
-        $(".insert-gifs > a[href='']").removeAttr('href');
-    })
-})
+	$('.menu-btn').click(() => {
+		$('.toggle-menu').toggle();
+	});
+
+	$(window).resize(() => {
+		if ($(this).width() > 571) {
+			$('.toggle-menu').show();
+		}
+	});
+
+	$('.search-it > .search-btn').on('click', function () {
+		const userInput = $(this).siblings().val();
+		if (userInput === '') {
+			$('.search-it > input').focus();
+		} else {
+			$('#gif-search').text(userInput);
+			// remove gifs from previous search
+			$('.source-link').remove();
+			$.ajax({
+				url: `https://api.giphy.com/v1/gifs/search?api_key=sL5xWiHQ5a6PSB0LoE6A6t2yovo19jxp&q=${userInput}&limit=30`,
+			}).done((res) => {
+				const { data } = res;
+				$.each(data, (i, { source, title, images: { original } }) => {
+					$('.insert-gifs').append(
+						`<a href="${source}" target="_blank" class="source-link"><img src="${original.url}" class="new-img" alt="${title}"/></a>`
+					);
+				});
+				$('#gif-items').text(`${data.length} GIFs`);
+			});
+			$(this).siblings().val('');
+		}
+	});
+	// Remove href attribute for gifs with no source
+	$('.insert-gifs').on('mouseover mouseenter', () => {
+		$('.insert-gifs > a[href=""]').removeAttr('href');
+	});
+});
